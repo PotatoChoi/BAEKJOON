19928cfb17108719f9c771c918e0d7d4f1b18179
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-07-21 08:57:44 +0900

    [Bronze V] Title: X보다 작은 수, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.py
@@ -1,6 +1,5 @@
-import sys
 N, X = map(int, input().split())
-A = list(map(int, sys.stdin.readline().split()))
+A = list(map(int, input().split()))
 for i in range(0,len(A)):
     if A[i]<X:
         print(A[i],end=' ')
