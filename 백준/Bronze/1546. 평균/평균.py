2ecbaef342efb5c6e9954191c24679057dec3405
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-07-21 11:12:06 +0900

    [Bronze I] Title: 평균, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.py
+++ 백준/Bronze/1546. 평균/평균.py
@@ -1,5 +1,4 @@
-import sys
 N=int(input())
-Score = list(map(int, sys.stdin.readline().split()))
+Score = list(map(int, input().split()))
 total = [Score[i]/max(Score)*100 for i in range(N)]
 print(sum(total)/N)
