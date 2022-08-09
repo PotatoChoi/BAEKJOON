7faa96d30f1f916bcfe1212f3a0a1e21bfb0afad
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-08-09 13:29:26 +0900

    [Bronze II] Title: 분해합, Time: 76 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/2231. 분해합/분해합.py
+++ 백준/Bronze/2231. 분해합/분해합.py
@@ -1,6 +1,10 @@
 N = int(input())
+Del = len(str(N))*9
+Nori = N-Del
 Result = 0
-for i in range(N+1):
+if Nori < 0 :
+    Nori = 0
+for i in range(Nori, N+1):
     Ncon = i + sum(map(int, str(i)))
     if Ncon == N:
         Result = i
