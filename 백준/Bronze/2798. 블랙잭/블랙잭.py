1ee8b798067c8e52a0f05560852fff21c1e2df07
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-08-09 16:49:13 +0900

    [Bronze II] Title: 블랙잭, Time: 140 ms, Memory: 36188 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.py
+++ 백준/Bronze/2798. 블랙잭/블랙잭.py
@@ -1,9 +1,10 @@
 N, M = map(int, input().split())
 Table = list(map(int, input().split()))
+Len = len(Table)
 Result = []
-for i in range(0, len(Table)-2):
-    for j in range(i+1, len(Table)-1):
-        for k in range(j+1, len(Table)):
+for i in range(0, Len-2):
+    for j in range(i+1, Len-1):
+        for k in range(j+1, Len):
             if (Table[i] + Table[j] + Table[k]) <= M :
                 Result.append(Table[i] + Table[j] + Table[k])
 
