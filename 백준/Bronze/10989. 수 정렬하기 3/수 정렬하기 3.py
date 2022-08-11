89528053af927db81d9b26682c4fa56155d7530e
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-08-11 12:06:09 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 6292 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -5,5 +5,6 @@ for _ in range(int(Inp())):
     T[int(Inp())] += 1
     
 for i, j in enumerate(T):
+    k = f"{i}\n"
     for _ in range(j):
-        sys.stdout.write(f"{i}\n")
+        sys.stdout.write(k)
