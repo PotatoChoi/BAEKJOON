8e436ba687b0055b11eae860a37b90a0e3f1f317
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-08-18 17:30:25 +0900

    [Silver V] Title: 요세푸스 문제 0, Time: 80 ms, Memory: 30840 KB -BaekjoonHub

--- 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
+++ 백준/Silver/11866. 요세푸스 문제 0/요세푸스 문제 0.py
@@ -2,12 +2,12 @@ N, K = map(int, input().split())
 
 Nlist = [i for i in range(1, N+1)]
 
+i = 0
 print('<', end='')
 while Nlist:
-    for i in range(K-1) :
-        Nlist.append(Nlist[0])
-        Nlist.pop(0)
-    print(Nlist.pop(0), end='')
-    if Nlist :
+    i += K-1
+    print(Nlist.pop(i%len(Nlist)), end='')
+    i = i%(len(Nlist)+1)
+    if Nlist:
         print(', ', end='')
 print('>')
