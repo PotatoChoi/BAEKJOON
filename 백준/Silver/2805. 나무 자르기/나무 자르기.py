6e2ee377951100c890d9e1342c9ce6ebdf82f4f6
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-08-28 16:47:11 +0900

    [Silver II] Title: 나무 자르기, Time: 4788 ms, Memory: 148408 KB -BaekjoonHub

--- 백준/Silver/2805. 나무 자르기/나무 자르기.py
+++ 백준/Silver/2805. 나무 자르기/나무 자르기.py
@@ -2,9 +2,10 @@ import sys
 
 N, M = map(int, input().split())
 woods = list(map(int, input().split()))
+woods.sort()
 
 start = 1
-end = max(woods)
+end = woods[-1]
 
 while (start <= end):
     mid = (start + end) // 2
