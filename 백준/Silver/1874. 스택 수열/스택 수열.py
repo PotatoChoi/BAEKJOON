453be9159fff02a84a9db003e3d176dbec2314b3
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-09-01 10:18:20 +0900

    [Silver II] Title: 스택 수열, Time: 208 ms, Memory: 35552 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,23 @@
+import sys
+
+n = int(input())
+stack = []
+result = []
+count = 0
+No = False
+
+for _ in range(n):
+    num = int(sys.stdin.readline().rstrip())
+    while count < num:
+        count += 1
+        stack.append(count)
+        result.append('+')
+    if stack[-1] == num:
+        stack.pop()
+        result.append('-')
+    else:
+        No = True
+if No == True:
+    print('NO')
+else:
+    print(*result, sep='\n')
