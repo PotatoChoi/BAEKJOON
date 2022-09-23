16c97cceca9c586e0f5ff7a696483d7d81b5f6a8
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-09-23 15:31:30 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 10.3 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12943. 콜라츠 추측/콜라츠 추측.py
@@ -0,0 +1,17 @@
+def solution(num):
+    answer = 0
+    while num != 1 :
+        if num == 0 :
+            answer = 0
+            break
+        if num % 2 == 0 :
+            num = num / 2
+        else :
+            num = num * 3 + 1
+        answer += 1
+        if num == 1 :
+            break
+        if answer == 500 :
+            answer = -1
+            break
+    return answer
