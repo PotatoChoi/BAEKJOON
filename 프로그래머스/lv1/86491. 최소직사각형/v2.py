81682740cf327167653360cf67f4af2123174745
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-09-24 08:50:15 +0900

    Create v2.py

---
+++ 프로그래머스/lv1/86491. 최소직사각형/v2.py
@@ -0,0 +1,9 @@
+def solution(sizes):
+  answer = 0
+  xline = []
+  yline = []
+  for i in sizes :
+    xline.append(max(i))
+    yline.append(min(i))
+  answer = max(xline) * max(yline)
+  return answer
