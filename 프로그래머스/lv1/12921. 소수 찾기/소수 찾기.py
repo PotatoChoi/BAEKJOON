7978400990dece6b916cfa35b19ce0b6796b0cb3
PotatoChoi <67477952+PotatoChoi@users.noreply.github.com>
2022-09-24 11:40:08 +0900

    Update 소수 찾기.py

--- 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv1/12921. 소수 찾기/소수 찾기.py
@@ -1,12 +1,12 @@
 def solution(n):
     answer = 0
-    vali = [True] * (n + 1)
-    vali[0], vali[1] = False, False
+    vali = [1] * (n + 1)
+    vali[0], vali[1] = 0, 0
     for i in range(2, int(n ** 0.5) + 1) :
         if vali[i] :
             j = 2
             while i * j <= n :
-                vali[i*j] = False
+                vali[i*j] = 0
                 j += 1
-    answer = vali.count(True)
+    answer = vali.count(1)
     return answer
